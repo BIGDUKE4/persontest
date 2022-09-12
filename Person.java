5a27a7d909573a5cd89a906439880876b8f762c0
BIGDUKE4 <magn424n@stud.kea.dk>
2022-09-12 09:25:29 +0200

    TEST

---
+++ Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private double højde;
+
+    public Person (double højde){
+        this.højde = højde;
+
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Person{" +
+                "højde=" + højde +
+                '}';
+    }
+}
+
+}
